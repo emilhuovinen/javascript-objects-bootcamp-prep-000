b41e98e4fe4b399807eb081ad3ea1d55b89208e9
emil.huo <emil.huo@gmail.com>
2018-09-18 08:16:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,14 @@
 var playlist = {
-  
+  redHotChiliPeppers: "Snow",
+  ladyGaga: "Pokerface"
 };
+
+var updatePlaylist = function (obj, artist, title) {
+  return Object.assign({}, obj, { [artist]: title })
+};
+
+var removeFromPlaylist = function (artist) {
+  delete playlist.artist;
+  
+  return playlist
+}
