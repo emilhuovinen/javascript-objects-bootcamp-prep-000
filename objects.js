7a997f16763d0231b463044adab199f9df4186a5
emil.huo <emil.huo@gmail.com>
2018-09-18 08:23:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {
 ['Slowdive']: "Alison",
-['My Bloody Valentine']: "Pokerface"
+['My Bloody Valentine']: "Sometimes"
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
