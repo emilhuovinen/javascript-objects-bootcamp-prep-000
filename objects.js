593e5326b77768e1899144294ca7bb4a116e6a04
emil.huo <emil.huo@gmail.com>
2018-09-18 08:37:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
- return delete playlist.artistName;
+ return delete playlist[artistName];
   
 }
 
