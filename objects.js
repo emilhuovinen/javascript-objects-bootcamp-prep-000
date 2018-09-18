16056d422e94cae8d7021ba07f62738b04df8779
emil.huo <emil.huo@gmail.com>
2018-09-18 08:31:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
 var playlist = {
 ['Slowdive']: "Alison",
-['My Bloody Valentine']: "Sometimes",
+['My Bloody Valentine']: "Sometimes"
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { artistName: songTitle })
+  return Object.assign({}, playlist, { [artistName]: songTitle })
 }
 
 var removeFromPlaylist = function (artist) {
